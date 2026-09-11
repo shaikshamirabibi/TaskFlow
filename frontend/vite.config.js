5235f13f92943c065faa8b51da7ae6808647ee78
Shaik shamira bibi <shaikshamirabibi@gmail.com>
2026-09-11 22:43:50 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
     proxy: {
@@ -12,5 +13,9 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+
+  preview: {
+    allowedHosts: ['taskflow-1-252b.onrender.com']
   }
 });
